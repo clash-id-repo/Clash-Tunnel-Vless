1b29bc649617cd5b02a499545d9838576f30a9ec
CLASH <155312403+clash-id-repo@users.noreply.github.com>
2024-01-29 13:00:48 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -714,11 +714,11 @@ function getVLESSConfig(userIDs, hostName) {
 		output.push(`Host: ${hostName}`);
 		output.push(`UUID: ${userID}`);
 		output.push(`Port TLS: 443\n\n`);
-		output.push(`${separator}\nv2ray default ip ✅\n${separator}\n${vlessMain}\n${separator}`);
-		output.push(`${separator}\nv2ray with best ip ✅\n${separator}\n${vlessSec}\n${separator}`);
+		output.push(`${separator}\nv2ray default ip ✅\n${separator}\n${vlessMain}\n\n`);
+		output.push(`${separator}\nv2ray with best ip ✅\n${separator}\n${vlessSec}\n\n`);
 	});
-		output.push(`${separator}\n# Clash Proxy Provider (configuration format) ✅\nproxy-groups:\n  - name: UseProvider\n	type: select\n	use:\n	  - provider1\n	proxies:\n	  - Proxy\n	  - DIRECT\nproxy-providers:\n  provider1:\n	type: http\n	url: https://${hostName}/sub/${userIDArray[0]}?format=clash\n	interval: 3600\n	path: ./provider1.yaml\n	health-check:\n	  enable: true\n	  interval: 600\n	  # lazy: true\n	  url: http://www.gstatic.com/generate_204\n\n${hashSeparator}`);
-		output.push(`\n${separator}Clash-Meta (configuration format) ✅\n${separator}\n     - type: vless\n  name: ${hostName}\n  server: ${hostName}\n  port: 443\n  uuid: ${userID}\n  network: ws\n  tls: true\n  udp: false\n  sni: ${hostName}\n  client-fingerprint: chrome\n  ws-opts:\n    path: "/?ed=2048"\n    headers:\n      host: ${hostName}\n---------------------------------------------------------------\n\n`);
+		output.push(`${separator}\nClash Proxy Provider (configuration format) ✅\n${separator}\nproxy-groups:\n  - name: UseProvider\n	type: select\n	use:\n	  - provider1\n	proxies:\n	  - Proxy\n	  - DIRECT\nproxy-providers:\n  provider1:\n	type: http\n	url: https://${hostName}/sub/${userIDArray[0]}?format=clash\n	interval: 3600\n	path: ./provider1.yaml\n	health-check:\n	  enable: true\n	  interval: 600\n	  # lazy: true\n	  url: http://www.gstatic.com/generate_204\n\n`);
+		output.push(`${separator}\nClash Meta (configuration format) ✅\n${separator}\n     - type: vless\n  name: ${hostName}\n  server: ${hostName}\n  port: 443\n  uuid: ${userID}\n  network: ws\n  tls: true\n  udp: false\n  sni: ${hostName}\n  client-fingerprint: chrome\n  ws-opts:\n    path: "/?ed=2048"\n    headers:\n   host: ${hostName}\n${separator}\n\n`);
 	
  	// HTML Head with CSS
 	const htmlHead = `
