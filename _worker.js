b85d0723a55ea82a5a82c0853c5a834c9227734c
CLASH <155312403+clash-id-repo@users.noreply.github.com>
2024-03-10 01:37:06 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,9 +3,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '9ebf8a30-e354-471c-89b8-ad6ab494e6bc';
+let userID = '78838e2c-b7ed-4a2d-bcd7-c7b1ceb8da49';
 
-const proxyIPs = ['35.200.154.186'];
+const proxyIPs = ['103.175.27.29'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
