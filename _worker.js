78ffa6e857b3a5491e2ad7441f9dbbaeb9668cee
CLASH <155312403+clash-id-repo@users.noreply.github.com>
2024-06-27 10:06:00 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'ac53f6b2-23fd-47ec-866a-d201d7708a51';
 
-const proxyIPs = ['103.173.75.62'];
+const proxyIPs = ['139.162.60.187'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
